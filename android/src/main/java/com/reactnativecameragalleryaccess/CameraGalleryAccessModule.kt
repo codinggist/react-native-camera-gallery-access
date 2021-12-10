4108750c9e3523e0c8f7e826355d0931e759d5f2
Muhammad Afaq Riaz <afaq@rnssol.com>
2021-12-10 14:55:04 +0500

    feat: added camera and image capture usecase for android

--- android/src/main/java/com/reactnativecameragalleryaccess/CameraGalleryAccessModule.kt
+++ android/src/main/java/com/reactnativecameragalleryaccess/CameraGalleryAccessModule.kt
@@ -1,24 +1,45 @@
 package com.reactnativecameragalleryaccess
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import android.app.Activity
+import android.content.Intent
+import com.facebook.react.bridge.*
+import com.reactnativecameragalleryaccess.camera.CameraActivity
 
-class CameraGalleryAccessModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+class CameraGalleryAccessModule(reactContext: ReactApplicationContext) :
+  ReactContextBaseJavaModule(reactContext), ActivityEventListener {
 
-    override fun getName(): String {
-        return "CameraGalleryAccess"
-    }
+  var url = 200
+  private lateinit var promise: Promise
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
-    }
+  override fun getName(): String {
+    return "CameraGalleryAccess"
+  }
 
-    
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  fun multiply(a: Int, b: Int, promise: Promise) {
+
+    val intent = Intent(reactApplicationContext, CameraActivity::class.java)
+
+    this.promise = promise
+
+    currentActivity?.startActivityForResult(intent, CAMERA_IMAGE_REQUEST_CODE)
+
+//    promise.resolve(url)
+
+  }
+
+
+  companion object {
+    private const val CAMERA_IMAGE_REQUEST_CODE = 20
+  }
+
+  override fun onActivityResult(p0: Activity?, p1: Int, p2: Int, p3: Intent?) {
+    promise.resolve(4000)
+  }
+
+  override fun onNewIntent(p0: Intent?) {
+    TODO("Not yet implemented")
+  }
 }
